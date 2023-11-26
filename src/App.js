ace7736ef2de46f83680762a16b91c16e4e875b1
conedex <conedex@web.de>
2023-11-26 03:59:27 +0100

    0.1.6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Collapse } from "antd";
-import polygonLogo from './images/polygonlogo.png';
-import bitconeLogo from './images/bitcone192.png';
+import { Collapse, Modal, message } from "antd";
+import polygonLogo from "./images/polygonlogo.png";
+import bitconeLogo from "./images/bitcone192.png";
+import LOTTERY_ABI from "./abis/Lottery.json";
+import TOKEN_ABI from "./abis/Token.json";
 import "./App.css";
 
 const { ethers } = require("ethers");
@@ -10,635 +12,6 @@ const { Panel } = Collapse;
 
 const CONTRACT_ADDRESS = "0xB3bA5A82263728c0128649BBeF634f64c2865F86";
 const TOKEN_CONTRACT_ADDRESS = "0x80273525B1548EeA1f211f4218Cf30c1a7C86b25";
-const LOTTERY_ABI = [
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "_vrfCoordinator",
-        type: "address",
-      },
-      {
-        internalType: "address",
-        name: "_link",
-        type: "address",
-      },
-      {
-        internalType: "bytes32",
-        name: "_keyHash",
-        type: "bytes32",
-      },
-      {
-        internalType: "address",
-        name: "_token",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "_tokenPerEntry",
-        type: "uint256",
-      },
-      {
-        internalType: "address",
-        name: "_rewardWallet",
-        type: "address",
-      },
-      {
-        internalType: "address",
-        name: "_admin",
-        type: "address",
-      },
-    ],
-    stateMutability: "nonpayable",
-    type: "constructor",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "owner",
-        type: "address",
-      },
-    ],
-    name: "OwnableInvalidOwner",
-    type: "error",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "account",
-        type: "address",
-      },
-    ],
-    name: "OwnableUnauthorizedAccount",
-    type: "error",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: "address",
-        name: "previousOwner",
-        type: "address",
-      },
-      {
-        indexed: true,
-        internalType: "address",
-        name: "newOwner",
-        type: "address",
-      },
-    ],
-    name: "OwnershipTransferred",
-    type: "event",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: "address",
-        name: "user",
-        type: "address",
-      },
-      {
-        indexed: false,
-        internalType: "uint256",
-        name: "userId",
-        type: "uint256",
-      },
-    ],
-    name: "UserEntered",
-    type: "event",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        internalType: "address",
-        name: "winner",
-        type: "address",
-      },
-      {
-        indexed: false,
-        internalType: "uint256",
-        name: "prize",
-        type: "uint256",
-      },
-    ],
-    name: "WinnerSelected",
-    type: "event",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "_admin",
-        type: "address",
-      },
-    ],
-    name: "addAdmin",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    name: "admins",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "currentPool",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "declareWinner",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "enterLottery",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "generateRandomNumber",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "generatedRandomNumber",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "getCurrentPool",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "getLastPrize",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "getLastWinner",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "lastPrize",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "lastWinner",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "nextUserId",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "owner",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "bytes32",
-        name: "requestId",
-        type: "bytes32",
-      },
-      {
-        internalType: "uint256",
-        name: "randomness",
-        type: "uint256",
-      },
-    ],
-    name: "rawFulfillRandomness",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "_admin",
-        type: "address",
-      },
-    ],
-    name: "removeAdmin",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "renounceOwnership",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "rewardWallet",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "_tokenPerEntry",
-        type: "uint256",
-      },
-    ],
-    name: "setTokenPerEntry",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "token",
-    outputs: [
-      {
-        internalType: "contract IERC20",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "tokenPerEntry",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "newOwner",
-        type: "address",
-      },
-    ],
-    name: "transferOwnership",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    name: "userIds",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    name: "winnings",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "withdrawLink",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-];
-const TOKEN_ABI = [
-  { inputs: [], stateMutability: "nonpayable", type: "constructor" },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: "address",
-        name: "owner",
-        type: "address",
-      },
-      {
-        indexed: true,
-        internalType: "address",
-        name: "spender",
-        type: "address",
-      },
-      {
-        indexed: false,
-        internalType: "uint256",
-        name: "value",
-        type: "uint256",
-      },
-    ],
-    name: "Approval",
-    type: "event",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: "address",
-        name: "previousOwner",
-        type: "address",
-      },
-      {
-        indexed: true,
-        internalType: "address",
-        name: "newOwner",
-        type: "address",
-      },
-    ],
-    name: "OwnershipTransferred",
-    type: "event",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: true, internalType: "address", name: "from", type: "address" },
-      { indexed: true, internalType: "address", name: "to", type: "address" },
-      {
-        indexed: false,
-        internalType: "uint256",
-        name: "value",
-        type: "uint256",
-      },
-    ],
-    name: "Transfer",
-    type: "event",
-  },
-  {
-    inputs: [
-      { internalType: "address", name: "owner", type: "address" },
-      { internalType: "address", name: "spender", type: "address" },
-    ],
-    name: "allowance",
-    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "address", name: "spender", type: "address" },
-      { internalType: "uint256", name: "amount", type: "uint256" },
-    ],
-    name: "approve",
-    outputs: [{ internalType: "bool", name: "", type: "bool" }],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [{ internalType: "address", name: "account", type: "address" }],
-    name: "balanceOf",
-    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "decimals",
-    outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "address", name: "spender", type: "address" },
-      { internalType: "uint256", name: "subtractedValue", type: "uint256" },
-    ],
-    name: "decreaseAllowance",
-    outputs: [{ internalType: "bool", name: "", type: "bool" }],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "address", name: "spender", type: "address" },
-      { internalType: "uint256", name: "addedValue", type: "uint256" },
-    ],
-    name: "increaseAllowance",
-    outputs: [{ internalType: "bool", name: "", type: "bool" }],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "address", name: "to", type: "address" },
-      { internalType: "uint256", name: "amount", type: "uint256" },
-    ],
-    name: "mint",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "name",
-    outputs: [{ internalType: "string", name: "", type: "string" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "owner",
-    outputs: [{ internalType: "address", name: "", type: "address" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "renounceOwnership",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "symbol",
-    outputs: [{ internalType: "string", name: "", type: "string" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "totalSupply",
-    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "address", name: "to", type: "address" },
-      { internalType: "uint256", name: "amount", type: "uint256" },
-    ],
-    name: "transfer",
-    outputs: [{ internalType: "bool", name: "", type: "bool" }],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "address", name: "from", type: "address" },
-      { internalType: "address", name: "to", type: "address" },
-      { internalType: "uint256", name: "amount", type: "uint256" },
-    ],
-    name: "transferFrom",
-    outputs: [{ internalType: "bool", name: "", type: "bool" }],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [{ internalType: "address", name: "newOwner", type: "address" }],
-    name: "transferOwnership",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-];
 
 function App() {
   const [provider, setProvider] = useState(null);
@@ -651,6 +24,7 @@ function App() {
   const [errorMessage, setErrorMessage] = useState(null);
   const [wrongNetwork, setWrongNetwork] = useState(false);
   const [countdown, setCountdown] = useState("");
+  const [transactionHash, setTransactionHash] = useState("");
 
   useEffect(() => {
     if (window.ethereum) {
@@ -774,8 +148,31 @@ function App() {
       }
 
       // If allowance is sufficient, enter lottery
+      const loadingMessageKey = "loadingMessage";
+      message.loading({
+        content: "Processing transaction...",
+        key: loadingMessageKey,
+      });
       const tx = await contractWithSigner.enterLottery();
       await tx.wait();
+      message.success({
+        content: (
+          <>
+            Successfully entered the Lottery.
+            <a
+              href={`https://mumbai.polygonscan.com/tx/${tx.hash}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ marginLeft: "10px" }}
+            >
+              Transaction Receipt
+            </a>
+          </>
+        ),
+        key: loadingMessageKey,
+        duration: 60,
+      });
+      setTransactionHash(tx.hash);
       fetchLotteryInfo();
       setErrorMessage(null); // clear any previous error messages
     } catch (error) {
@@ -794,28 +191,51 @@ function App() {
     }
   }, [contract]);
 
+  function formatNumber(num) {
+    return new Intl.NumberFormat().format(Math.floor(num));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-      {wrongNetwork ? (
-        <button onClick={switchNetwork}>Wrong Network</button>
-      ) : account ? (
-        <>
-          <img src={polygonLogo} alt="Polygon Image" className="wallet-image" style={{ marginRight: '10px' }} />
-          {account.substring(0, 5) +
-            "..." +
-            account.substring(account.length - 3)}
-        </>
-      ) : (
-        <button onClick={connectWallet}>Connect Wallet</button>
-      )}
-    </header>
+        {wrongNetwork ? (
+          <button onClick={switchNetwork}>Wrong Network</button>
+        ) : account ? (
+          <>
+            <img
+              src={polygonLogo}
+              alt="Polygon Image"
+              className="wallet-image"
+              style={{ marginRight: "10px" }}
+            />
+            {account.substring(0, 5) +
+              "..." +
+              account.substring(account.length - 3)}
+          </>
+        ) : (
+          <button onClick={connectWallet}>Connect Wallet</button>
+        )}
+      </header>
       <main>
-        <h1><img src={bitconeLogo} alt="Bitcone Logo Left" className="bitcone-header" style={{ marginRight: '1%', marginLeft: '1%' }} />BitCone Lottery<img src={bitconeLogo} alt="Bitcone Logo Left" className="bitcone-header" style={{ marginRight: '1%', marginLeft: '1%' }} /></h1>
+        <h1>
+          <img
+            src={bitconeLogo}
+            alt="Bitcone Logo Left"
+            className="bitcone-header"
+            style={{ marginRight: "1%", marginLeft: "1%" }}
+          />
+          BitCone Lottery
+          <img
+            src={bitconeLogo}
+            alt="Bitcone Logo Left"
+            className="bitcone-header"
+            style={{ marginRight: "1%", marginLeft: "1%" }}
+          />
+        </h1>
         <div className="lottery-info">
           <p>Next pull in: {countdown}</p>
-          <p>Amount in current Lottery: {currentPool} CONE</p>
-          <p>Entry Amount: 1,000,000 CONE</p>
+          <p>Amount in current Lottery: {formatNumber(currentPool)} CONE</p>
+          <p>Entry Amount: 1.000.000 CONE</p>
           {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
           <button onClick={enterLottery} disabled={!account}>
             Enter Lottery
