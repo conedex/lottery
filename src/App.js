afc551a584c6f39f94a7ec6b62a9bd5f0f03c50b
joshschcom <joshuaschiemann@yahoo.de>
2023-12-19 13:31:23 +0100

    0.1.9

--- src/App.js
+++ src/App.js
@@ -276,7 +276,7 @@ function App() {
         <div className="lottery-info">
           <p>Next pull in: {countdown}</p>
           <p>Amount in current Lottery: {formatNumber(currentPool)} CONE</p>
-          <p>Entry Amount: 1.000.000 CONE</p>
+          <p>Entry Amount: 100.000 CONE</p>
           {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
           <button onClick={enterLottery} disabled={!account}>
             Enter Lottery
