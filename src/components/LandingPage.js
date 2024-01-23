eb16ddba1a060e6244cda23395256a81f2431a4b
joshschcom <joshuaschiemann@yahoo.de>
2024-01-23 13:01:04 +0100

    0.2.3

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -11,6 +11,7 @@ const LandingPage = () => {
     <div>
       <div className="banner">
         <a
+          className="banner-link"
           href="https://rccmarketcap.com/"
           target="_blank"
           rel="noopener noreferrer"
